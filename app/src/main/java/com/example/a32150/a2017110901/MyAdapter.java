8fac4853fd4437394b126bd4c61eb25c55533391
deh3215 <3215009@gmail.com>
2017-11-09 12:48:06 +0800

    高級,省資源的寫法~~~~~

--- app/src/main/java/com/example/a32150/a2017110901/MyAdapter.java
+++ app/src/main/java/com/example/a32150/a2017110901/MyAdapter.java
@@ -2,12 +2,17 @@ package com.example.a32150.a2017110901;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -18,14 +23,18 @@ import org.w3c.dom.Text;
 public class MyAdapter extends BaseAdapter {
 
     Context context;
+    public String str[];
+    public boolean chk[];
 
-    public MyAdapter(Context context)  {
+    public MyAdapter(Context context, String[] str)  {
         this.context = context;
+        this.str = str;
+        chk = new boolean[str.length];
     }
 
     @Override
     public int getCount() {
-        return 5;
+        return str.length;
     }
 
     @Override
@@ -39,7 +48,7 @@ public class MyAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int i, View view, ViewGroup viewGroup) {
 //        Method-1
 //        TextView tv=new TextView(context);
 //        tv.setTextSize(32);
@@ -48,15 +57,57 @@ public class MyAdapter extends BaseAdapter {
 //        tv.setBackgroundColor(Color.CYAN);
 //        return tv;
 
-        //Method-2  自訂Layout
+//      Method-2  自訂Layout
+        Log.d("GetView", "Position="+i);
+
+        ViewHolder holder;
         LayoutInflater inflator = LayoutInflater.from(context);
-        View v=inflator.inflate(R.layout.myitem, null);//Layout 解壓器
-        TextView tv = (TextView)v.findViewById(R.id.textView);
-        tv.setTextSize(32);
-        tv.setText("Hi Micky"+i);
+        if(view == null)    {
+            view = inflator.inflate(R.layout.myitem, null);//解出Layout 解壓器,消耗資源
+            holder = new ViewHolder();
+            holder.btn = (Button)view.findViewById(R.id.button);
+            holder.tv = (TextView)view.findViewById(R.id.textView);
+            holder.chk = (CheckBox)view.findViewById(R.id.checkBox);
+            view.setTag(holder);//要加,不然listview滑動會當掉
+        }   else    {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        holder.tv.setTextSize(48);
+        holder.tv.setText(str[i]);
+
+        holder.btn.setText(str[i]);
+        holder.btn.setTextSize(48);
+
+        final String msg=str[i];
+
+        holder.btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+             chk[i]=b;
+           }
+       });
+        holder.chk.setChecked(chk[i]);
+
+//========以上resoure會經由回收機制,重複被使用,所以view的元件沒被更新的話,原本元件的狀態會被保留住,除非更新=======
 
 
-
-        return tv;
+        return view;
     }
+
+    static class ViewHolder
+    {
+        TextView tv;
+        Button btn;
+        CheckBox chk;
+    }
+
+
 }
